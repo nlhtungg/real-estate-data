80e55007c2bad8f36d810d7b3646b22690e64f4b
nlhtungg <nlhtungg@gmail.com>
2025-08-19 21:19:31 +0700

    using airflow to schedule spark job

--- dags/dag.py
+++ dags/dag.py
@@ -17,8 +17,7 @@ with DAG(
     default_args=default_args,
     start_date=datetime(2025, 8, 4, tzinfo=local_tz),
     schedule_interval="0 9 * * *",  # mỗi ngày lúc 9h sáng
-    catchup=False,
-    tags=["alonhadat", "crawler"]
+    catchup=False
 ) as dag:
 
     crawl_job = BashOperator(
@@ -26,8 +25,21 @@ with DAG(
         bash_command="python3 /opt/airflow/ingest/crawler.py"
     )
 
+    transform_job = BashOperator(
+        task_id='run_my_spark_application',
+        bash_command='''
+        docker exec spark-master spark-submit \
+            --master spark://spark-master:7077 \
+            --conf spark.executor.memory=2g \
+            --conf spark.driver.memory=1g \
+            --name airflow-spark-job \
+            --deploy-mode client \
+            /opt/transform/transform.py
+        '''
+    )
+
     success = DummyOperator(task_id="success")
     fail = DummyOperator(task_id="fail", trigger_rule="one_failed")
 
-    crawl_job >> success
-    crawl_job >> fail
+    crawl_job >> transform_job >> success
+    [crawl_job, transform_job] >> fail
